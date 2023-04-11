022b20df3f6fbae68d60fef91b1b07b3fb691e20
1704711 <xadowknight@gmail.com>
2023-04-11 09:22:56 +0100

    Chamar a função enviaMensagem mediante o botão enviar (Adicionar Evento onClick para o botão enviar)

--- app/src/main/java/com/example/mensagem/MainActivity.kt
+++ app/src/main/java/com/example/mensagem/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mensagem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun enviaMensagem() {
-
+        Toast.makeText(this, "Enviar Mensagem", Toast.LENGTH_SHORT).show()
     }
 }
